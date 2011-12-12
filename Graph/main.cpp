8fc5f26566d5b7cca9748806b32186ccc661dd81
Donjan Rodic <bryonak@freenet.de>
2011-12-12 17:56:38 +0100

    cleanup

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -6,12 +6,8 @@ int main()
 {
 
 	PeriodicCartesian<3> graph(1,2,3);
-<<<<<<< HEAD
-	//~ boost::AdjacencyGraphConcept< PeriodicCartesian<3> > concept;
-=======
 	boost::GraphConcept< PeriodicCartesian<3> > concept;
->>>>>>> e5da9d8415a06adff69355ef186412a501d9202d
-	
+
 	return 0;
 
 }
