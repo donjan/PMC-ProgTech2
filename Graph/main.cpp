924b2fbbe9bb3766eb5293d124cde736d34460a5
Rico Häuselmann <r.haeuselmann@gmx.ch>
2011-12-12 17:38:40 +0100

    Revert "Revert 49a1f599d91dd18f5d3bdf8913d0c0a0fca47d85^..HEAD"
    This reverts commit 08c63f4dfdd04f7709924c054e6b725c4354b847.

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -4,8 +4,11 @@
 
 int main(int argc, char *argv[])
 {
+  
+  if(argc>10) std::cout <<argv[1][0];
+
 	PeriodicCartesian<3> graph(1,2,3);
-	//boost::AdjacencyGraphConcept< PeriodicCartesian<3> > concept;
+	boost::AdjacencyGraphConcept< PeriodicCartesian<3> > concept;
 	
 	return 0;
 
