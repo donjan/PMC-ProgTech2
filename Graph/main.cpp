d893e37a8c6aece66d5d646b4bcf172110b31531
Donjan Rodic <bryonak@freenet.de>
2011-12-12 17:50:26 +0100

    Moving modular accum to separate dir

--- Graph/main.cpp
+++ Graph/main.cpp
@@ -2,7 +2,7 @@
 #include "implicit_cartesian.hpp"
 #include <boost/graph/graph_concepts.hpp>
 
-int main(int argc, char *argv[])
+int main()
 {
 	PeriodicCartesian<3> graph(1,2,3);
 	boost::AdjacencyGraphConcept< PeriodicCartesian<3> > concept;
